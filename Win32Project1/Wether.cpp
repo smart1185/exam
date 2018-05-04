c22cc8deac31360014da76ec24bfa3d058b65ad0
smart1185 <34085686+smart1185@users.noreply.github.com>
2018-05-04 21:23:18 +0600

    Update Wether.cpp

--- Win32Project1/Wether.cpp
+++ Win32Project1/Wether.cpp
@@ -17,10 +17,30 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	
-	Weather *wet = (Weather*)malloc(sizeof(Weather) * 100);
+	Weather *wet = (Weather*)malloc(sizeof(Weather));
+	Buf *b=(Buf*)malloc(sizeof(Buf));
 	
+	char *c;	
 	W = fopen("1.csv", "r");
 	
+	int i=0;
+	while (1)
+	{
+		b=(Buf*)realloc(b,(i+1)*sizeof(Buf));
+		(b+i)->buf=(char*)malloc(sizeof(char)*500);
+		c=fgets((b+i)->buf, sizeeof(char)*500,W);
+		if (c==NULL)
+        {
+            if(feof(W)!=0)
+                break;
+            else{
+                printf("\nReading error\n");
+                break;
+            }
+        }
+        i++;
+	}
+
 	
 	
 	fclose(W);
